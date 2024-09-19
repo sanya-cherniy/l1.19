d9edda864d5df72f024310324798237b3e11cfc6
janellca <janellca@student.21-school.ru>
2024-09-20 04:56:18 +0700

    release

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ fn main() {
     io::stdin().read_line(&mut input).unwrap(); // считываем строку
     let string = input.as_str(); // преобразуем в строковый срез
 
-    let words = string.split_word_bounds().collect::<Vec<&str>>();
+    let words = string.split_word_bounds().collect::<Vec<&str>>(); // разбиваем строку на слова
     let mut result = String::new();
+
     for &word in words.iter().rev() {
         // записываем полученные слова в результирующую строку в обратном порядке
         result.push_str(word);
