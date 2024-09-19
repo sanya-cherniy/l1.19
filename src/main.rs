4eb9be1c644786b1a3f4fd03d056ace0cc769163
janellca <janellca@student.21-school.ru>
2024-09-20 04:55:02 +0700

    release

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ use unicode_segmentation::UnicodeSegmentation; // испольуем крейт 
 fn main() {
     let mut input = String::new();
 
-    io::stdin().read_line(&mut input); // считываем строку
+    io::stdin().read_line(&mut input).unwrap(); // считываем строку
     let string = input.as_str(); // преобразуем в строковый срез
-                                 // let graphemes = UnicodeSegmentation::graphemes(string, true).collect::<Vec<&str>>(); // разбиваем строку на графемы
+
     let words = string.split_word_bounds().collect::<Vec<&str>>();
     let mut result = String::new();
     for &word in words.iter().rev() {
-        // записываем полученные графемы в результирующую строку в обратном порядке
+        // записываем полученные слова в результирующую строку в обратном порядке
         result.push_str(word);
     }
     println!("{}", result);
